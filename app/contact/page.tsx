4b689481af279fcca9eef0dc824a76478335bcf2
Jomarie Esguerra <jarreugse09@gmail.com>
2026-01-14 12:21:42 +0800

    delete contact page

--- app/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/contact/page.tsx
-export default function ContactPage() {
-  return (
-    <main className="min-h-screen flex items-center justify-center bg-black text-white">
-      <h1 className="text-4xl terminal-text">Contact</h1>
-    </main>
-  );
-}
