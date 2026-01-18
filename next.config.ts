858537ac5b499d177029f566a0c261e6f4bb3ccb
Jomarie Esguerra <jarreugse09@gmail.com>
2026-01-19 00:21:12 +0800

    config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  turbopack: {
-    // Explicitly set the project root to avoid lockfile-based inference
-    root: __dirname,
-  },
 };
 
 module.exports = nextConfig;
